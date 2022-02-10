efb288988afc993cf8899e7d454529518f75081d
WasokSriparat <sriparat123@gmail.com>
2022-02-10 23:43:41 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config({path:'./config.env'});
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = process.env.PORT || 4002;
+
+const staffRoute = require("./routes/staffRoute");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+require("./db")(app);
+
+// localhost:4000/staff
+app.use("/staff",staffRoute);
+
+app.get("/",(req,res)=>{
+    res.send("Hello Index");
+});
+
+app.listen(port,()=>{
+    console.log("App is running on port " + port);
+});
